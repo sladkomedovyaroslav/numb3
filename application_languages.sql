44060af2a93194cdca3bce32ab86d121cbea8cb8
sladkomedovyaroslav <sladkomedov.yaroslav10@gmail.com>
2026-05-22 01:23:07 +0300

    Delete application_languages.sql

--- application_languages.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE application_languages (
-    application_id INT(10) UNSIGNED NOT NULL,
-    language_id INT(10) UNSIGNED NOT NULL,
-    FOREIGN KEY (application_id) REFERENCES applications(id) ON DELETE CASCADE,
-    FOREIGN KEY (language_id) REFERENCES programming_languages(id),
-    PRIMARY KEY (application_id, language_id)
-);
